77efbe01ec5819fe7992a21c96aa8445d5dbe32e
FilipSwiatek <filipswiatek.fil@gmail.com>
2020-03-18 01:12:27 +0100

    added OLED and encoder support

---
+++ Core/Inc/devices.h
@@ -0,0 +1,45 @@
+/*
+ * devices.h
+ *
+ *  Created on: Mar 17, 2020
+ *      Author: Snickers
+ */
+
+#ifndef INC_INTERFACES_H_
+#define INC_DEVICES_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#define SWITCH_1_PIN GPIO_PIN_0
+#define SWITCH_2_PIN GPIO_PIN_1
+#define SWITCH_3_PIN GPIO_PIN_2
+#define SWITCH_4_PIN GPIO_PIN_10
+#define POT_SWITCH_PIN GPIO_PIN_11
+#define STATUS_LED_PIN GPIO_PIN_13
+
+#define SWITCH_1_PORT GPIOB
+#define SWITCH_2_PORT GPIOB
+#define SWITCH_3_PORT GPIOB
+#define SWITCH_4_PORT GPIOB
+#define ENC_SWITCH_PORT GPIOB
+#define STATUS_LED_PORT GPIOC
+
+
+int8_t EncoderGetOffset(void);
+void BuzzerBeep(void);
+void HeaterSetDutyCycle(uint8_t percent);
+uint16_t GetCurrentTemperature();
+
+// basic interfaces
+
+bool GPIO_Init(void);
+bool ADC1_Init(void);
+bool I2C1_Init(void);
+bool TIM1_Init(void);
+bool TIM2_Init(void);
+bool USART1_UART_Init(void);
+bool TIM3_Init(void);
+void StatusLED_Proc(void);
+
+#endif /* INC_DEVICES_H_ */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,60 +21,31 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include <stdbool.h>
+#include "ssd1306.h"
+#include "devices.h"
 
-#define SWITCH_1_PIN GPIO_PIN_0
-#define SWITCH_2_PIN GPIO_PIN_1
-#define SWITCH_3_PIN GPIO_PIN_2
-#define SWITCH_4_PIN GPIO_PIN_10
-#define POT_SWITCH_PIN GPIO_PIN_11
-#define STATUS_LED_PIN GPIO_PIN_13
-
-#define SWITCH_1_PORT GPIOB
-#define SWITCH_2_PORT GPIOB
-#define SWITCH_3_PORT GPIOB
-#define SWITCH_4_PORT GPIOB
-#define ENC_SWITCH_PORT GPIOB
-#define STATUS_LED_PORT GPIOC
-
-
-
-ADC_HandleTypeDef hadc1;
-I2C_HandleTypeDef hi2c1;
-TIM_HandleTypeDef htim1;
-TIM_HandleTypeDef htim2;
-TIM_HandleTypeDef htim3;
-UART_HandleTypeDef huart1;
-
-
-// function prototypes
 bool SystemClock_Config(void);
-static bool MX_GPIO_Init(void);
-static bool MX_ADC1_Init(void);
-static bool MX_I2C1_Init(void);
-static bool MX_TIM1_Init(void);
-static bool MX_TIM2_Init(void);
-static bool MX_USART1_UART_Init(void);
-static bool MX_TIM3_Init(void);
-
-static void StatusLED_Proc(void);
 
 
 int main(void){
   HAL_Init();
   if(!SystemClock_Config()) Error_Handler();
 
-  if(!MX_GPIO_Init()) Error_Handler();
-  if(!MX_ADC1_Init()) Error_Handler();
-  if(!MX_I2C1_Init()) Error_Handler();
-  if(!MX_TIM1_Init()) Error_Handler();
-  if(!MX_TIM2_Init()) Error_Handler();
-  if(!MX_TIM3_Init()) Error_Handler();
-  if(!MX_USART1_UART_Init()) Error_Handler();
+  if(!GPIO_Init()) Error_Handler();
+  if(!ADC1_Init()) Error_Handler();
+  if(!I2C1_Init()) Error_Handler();
+  if(!TIM1_Init()) Error_Handler();
+  if(!TIM2_Init()) Error_Handler();
+  if(!TIM3_Init()) Error_Handler();
+  if(!USART1_UART_Init()) Error_Handler();
+  ssd1306_Init();
+  ssd1306_Fill(White);
+  ssd1306_UpdateScreen();
   while (1){
 	  StatusLED_Proc();
+	  EncoderGetOffset();
   }
 }
-
 
 bool SystemClock_Config(void){
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -113,263 +84,12 @@ bool SystemClock_Config(void){
   return true;
 }
 
-
-static bool MX_ADC1_Init(void){
-  ADC_ChannelConfTypeDef sConfig = {0};
-  hadc1.Instance = ADC1;
-  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
-  hadc1.Init.ContinuousConvMode = DISABLE;
-  hadc1.Init.DiscontinuousConvMode = DISABLE;
-  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 1;
-  if (HAL_ADC_Init(&hadc1) != HAL_OK)
-  {
-    return false;
-  }
-  /** Configure Regular Channel 
-  */
-  sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
-  sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    return false;
-  }
-  return true;
-}
-
-
-static bool MX_I2C1_Init(void){
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 100000;
-  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
-    return false;
-  }
-return true;
-}
-
-
-static bool MX_TIM1_Init(void)
-{
-
-  TIM_Encoder_InitTypeDef sConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-
-  htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 0;
-  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 0;
-  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim1.Init.RepetitionCounter = 0;
-  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  sConfig.EncoderMode = TIM_ENCODERMODE_TI1;
-  sConfig.IC1Polarity = TIM_ICPOLARITY_RISING;
-  sConfig.IC1Selection = TIM_ICSELECTION_DIRECTTI;
-  sConfig.IC1Prescaler = TIM_ICPSC_DIV1;
-  sConfig.IC1Filter = 0;
-  sConfig.IC2Polarity = TIM_ICPOLARITY_RISING;
-  sConfig.IC2Selection = TIM_ICSELECTION_DIRECTTI;
-  sConfig.IC2Prescaler = TIM_ICPSC_DIV1;
-  sConfig.IC2Filter = 0;
-  if (HAL_TIM_Encoder_Init(&htim1, &sConfig) != HAL_OK){
-    return false;
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK){
-    return false;
-  }
-return true;
-}
-
-/**
-  * @brief TIM2 Initialization Function
-  * @param None
-  * @retval None
-  */
-static bool MX_TIM2_Init(void){
-
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_OC_InitTypeDef sConfigOC = {0};
-
-  htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 0;
-  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 0;
-  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim2) != HAL_OK){
-    return false;
-  }
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK){
-    return false;
-  }
-  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK){
-	  return false;
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
-  {
-    return false;
-  }
-  sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 0;
-  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_3) != HAL_OK){
-    return false;
-  }
-  HAL_TIM_MspPostInit(&htim2);
-return true;
-}
-
-static bool MX_TIM3_Init(void){
-
-  /* USER CODE BEGIN TIM3_Init 0 */
-
-  /* USER CODE END TIM3_Init 0 */
-
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_OC_InitTypeDef sConfigOC = {0};
-
-  /* USER CODE BEGIN TIM3_Init 1 */
-
-  /* USER CODE END TIM3_Init 1 */
-  htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 0;
-  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 0;
-  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
-  {
-    return false;
-  }
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
-  {
-	  return false;
-  }
-  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
-  {
-	  return false;
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
-  {
-	  return false;
-  }
-  sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 0;
-  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
-  {
-	  return false;
-  }
-  /* USER CODE BEGIN TIM3_Init 2 */
-
-  /* USER CODE END TIM3_Init 2 */
-  HAL_TIM_MspPostInit(&htim3);
-  return true;
-
-}
-
-
-static bool MX_USART1_UART_Init(void)
-{
-  huart1.Instance = USART1;
-  huart1.Init.BaudRate = 115200;
-  huart1.Init.WordLength = UART_WORDLENGTH_8B;
-  huart1.Init.StopBits = UART_STOPBITS_1;
-  huart1.Init.Parity = UART_PARITY_NONE;
-  huart1.Init.Mode = UART_MODE_TX_RX;
-  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart1) != HAL_OK){
-    return false;
-  }
-return true;
-}
-
-
-
-
-static bool MX_GPIO_Init(void){
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-  //buttons' and status led's  GPIOs clock enable
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-
-  // initialize buttons
-  GPIO_InitStruct.Pin = SWITCH_1_PIN;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(SWITCH_1_PORT, &GPIO_InitStruct);
-
-  GPIO_InitStruct.Pin = SWITCH_2_PIN;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(SWITCH_2_PORT, &GPIO_InitStruct);
-
-  GPIO_InitStruct.Pin = SWITCH_3_PIN;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(SWITCH_3_PORT, &GPIO_InitStruct);
-
-  GPIO_InitStruct.Pin = SWITCH_4_PIN;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(SWITCH_4_PORT, &GPIO_InitStruct);
-
-  GPIO_InitStruct.Pin = POT_SWITCH_PIN;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(ENC_SWITCH_PORT, &GPIO_InitStruct);
-
-  //initialize status diode
-  GPIO_InitStruct.Pin = STATUS_LED_PIN;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_WritePin(STATUS_LED_PORT, STATUS_LED_PIN, GPIO_PIN_SET); // initialize diode as turned off
-  HAL_GPIO_Init(STATUS_LED_PORT, &GPIO_InitStruct);
-return true;
-}
-
-
-
 void Error_Handler(void){
-	//turn on status diode
-	HAL_GPIO_WritePin(STATUS_LED_PORT, STATUS_LED_PIN, GPIO_PIN_RESET); // turn on status diode
-	//infinite loop
+	HAL_GPIO_WritePin(STATUS_LED_PORT, STATUS_LED_PIN, GPIO_PIN_RESET); // turn status diode on
 	while(1);
 }
 
-static void StatusLED_Proc(void){
-	static uint32_t previousCallTimestamp;
-	if((HAL_GetTick() - previousCallTimestamp) > 1000 ){
-		HAL_GPIO_TogglePin(STATUS_LED_PORT, STATUS_LED_PIN); // toggle status diode state
-		previousCallTimestamp = HAL_GetTick();
-	}
-}
+
 
 #ifdef  USE_FULL_ASSERT
 /**
